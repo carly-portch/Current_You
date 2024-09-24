868b0f0a8b49a4386bd8729133b0f911a3fd8714
carly-portch <carly.portch@outlook.com>
2024-09-24 17:22:15 -0400

    Update current_you.py

--- current_you.py
+++ current_you.py
@@ -207,7 +207,7 @@ def main():
 
         if difference <= 0:
             st.markdown("<h2 class='section-header'>Great news! Your expenses are ${:.2f} under your Future You limit. This means you are on track to achieve the future you want and even have some extra money you can allocate to having fun, additional goals or leveling up your fixed expenses!</h2>".format(abs(difference)), unsafe_allow_html=True)
-        if difference == 0:
+        elif difference == 0:
             st.markdown("<h2 class='section-header'>Great news! Your expenses are equal to your Future You limit. This means you are right on track to achieving the future you want if you keep your expenses to the limits you have defined above.</h2>".format(difference), unsafe_allow_html=True)
         else:
             st.markdown("<h2 class='section-header'>Uh oh! Your expenses are ${:.2f} over your Future You limit. This means that you are spending more than what is required to reach your Future You goals.</h2>".format(difference), unsafe_allow_html=True)
